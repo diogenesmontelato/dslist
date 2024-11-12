20f8c4a918c01b88bbb451f2d3fa7297a72cf35e
Diogenes Montelato <diogenes.montealto@gmail.com>
2024-11-12 01:06:58 -0300

    GameMinDTO, GameRepository, GameService

---
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dslist.services;
+
+import org.springframework.stereotype.Service;
+
+//PODE SER USADO O @Component TAMBEM - SERVER PARA REGISTRAR O GAMERSERVICE COMO UM COPONENTE DO SISTEMA
+@Service
+public class GameService {
+
+}
